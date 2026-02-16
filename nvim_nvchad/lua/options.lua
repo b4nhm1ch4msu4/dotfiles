20ed841b8e1fab01e2f1dbd9437ee21f158f8292
Laptop HP <tienpham102vp@gmail.com>
2026-02-16 19:08:54 +0700

    remote suckless from dotfile, add lazyvim

--- nvim_nvchad/lua/options.lua
+++
@@ -1,7 +0,0 @@
-require "nvchad.options"
--- add yours here!
-vim.opt.whichwrap = "b,s"
-vim.o.relativenumber = true
-
--- add border on float window of lsp signature
-vim.o.winborder = "bold"
--- suckless/scripts/current_battery_level.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
-notify-send "Battery level: $battery_level%"
