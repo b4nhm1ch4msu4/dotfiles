4c50d678297b55b16ae97d3f10dc78d3b6c7266f
Tien_Pham <85607646+b4nhm1ch4msu4@users.noreply.github.com>
2026-02-26 10:58:38 +0700

    Add configuration for blink.cmp plugin

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,13 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "default",
+      ["C-y"] = { "accept" },
+      ["<Tab>"] = {},
+      ["<S-Tab>"] = {},
+      -- ['<PageUp>'] = { 'snippet_forward', 'fallback' },
+      -- ['<PageDown>'] = { 'snippet_backward', 'fallback' },
+    },
+  },
+}
