168f2268c0660f6d8fb3a34ff30be4d7d31f5030
Laptop HP <tienpham102vp@gmail.com>
2026-03-10 22:53:03 +0700

    suckless script: incress low batt warring, add option to monitor setup script

--- suckless/scripts/low_battery_warning.sh
+++ suckless/scripts/low_battery_warning.sh
@@ -3,7 +3,7 @@
 while true; do
     level=$(cat /sys/class/power_supply/BAT0/capacity)
     status=$(cat /sys/class/power_supply/BAT0/status)
-    if [ "$level" -le 15 ] && [ "$status" = "Discharging" ]; then
+    if [ "$level" -le 20 ] && [ "$status" = "Discharging" ]; then
         notify-send "Low Battery" "${level}%" -u critical
     fi
     sleep 60
--- suckless/scripts/setup_monitors.sh
+++ suckless/scripts/setup_monitors.sh
@@ -9,8 +9,19 @@ if [ "$1" = 1 ];then
 elif [ "$1" = 2 ]; then
   xrandr --output "$internal_mon" --off \
     --output "$external_mon" --mode "$res" --auto 
+elif [ "$1" = "right" ]; then
+  xrandr --output "$internal_mon" --mode "$res" --auto \
+    --output "$external_mon" --mode "$res" --auto --right-of "$internal_mon" --primary
+elif [ "$1" = "left" ]; then
+  xrandr --output "$internal_mon" --mode "$res" --auto \
+    --output "$external_mon" --mode "$res" --auto --left-of "$internal_mon" --primary
+elif [ "$1" = "below" ]; then
+  xrandr --output "$internal_mon" --mode "$res" --auto \
+    --output "$external_mon" --mode "$res" --auto --below "$internal_mon" --primary
+elif [ "$1" = "above"]
+  xrandr --output "$internal_mon" --mode "$res" --auto \
+    --output "$external_mon" --mode "$res" --auto --above "$internal_mon" --primary
 else
   xrandr --output "$internal_mon" --mode "$res" --auto \
     --output "$external_mon" --mode "$res" --auto --above "$internal_mon" --primary
 fi
-
