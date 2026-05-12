62a01915e9c0aa8965563d1f60368e15dfbb00d6
Laptop HP <tienpham102vp@gmail.com>
2026-05-12 21:40:14 +0700

    add keymap C-a and C-c

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,9 +9,11 @@ map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "previous buffer" })
 
 -- useful edit keymaps
 map("x", "<leader>p", [["_dP]], { desc = "Primeagen Copy" })
+map("n", "<C-a>", "ggVG", { desc = "Select all" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll plus zz" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Scroll plus zz" })
 map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Line Up" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Line Down" })
+map({ "n", "i", "v" }, "<C-c>", "<Esc>", { desc = "Esc" })
