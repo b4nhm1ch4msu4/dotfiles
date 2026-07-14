8d234cffc84a54a77f6dd285dd7b0292363d77e0
tien2.pham <tien2.pham@lge.com>
2026-07-14 09:59:46 +0700

    tien2.pham add robotframework lsp config

--- nvim_lazy/lua/plugins/lsp.lua
+++ nvim_lazy/lua/plugins/lsp.lua
@@ -49,6 +49,24 @@ return {
 					},
 				},
 			},
+			robotframework_ls = {
+				settings = {
+					robot = {
+						pythonpath = {
+							"C:\\workspace\\bmw_icon_resource\\resource",
+							-- "C:\\workspace\\bmw_icon_script\\Plugin",
+							"C:\\workspace\\bmw_icon_script\\Plugin\\EnvPlugin",
+							-- "C:\\workspace\\bmw_icon_script\\Plugin\\EnvPlugin\\dlt",
+							-- "C:\\workspace\\bmw_icon_script\\Plugin\\EnvPlugin\\SP25Bench",
+							-- "C:\\workspace\\bmw_icon_script\\Plugin\\EnvPlugin\\PCAP",
+							"C:\\workspace\\bmw_icon_script\\Plugin\\EnvPlugin\\oecon",
+							-- "C:\\workspace\\bmw_icon_script\\Plugin\\EnvPlugin\\canat",
+							-- "C:\\workspace\\bmw_icon_script\\Plugin\\EnvPlugin\\PCAN",
+							-- "C:\\workspace\\bmw_icon_script\\Plugin\\EnvPlugin\\Util",
+						},
+					},
+				},
+			},
 		}
 
 		local ensure_installed = vim.tbl_keys(servers or {})
