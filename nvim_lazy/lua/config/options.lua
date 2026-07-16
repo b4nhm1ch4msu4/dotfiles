4e1e019e5f7df855d13a4db442ad0dc023c13b16
tien2.pham <tien2.pham@lge.com>
2026-07-16 08:39:07 +0700

    tien2.pham add pwsh shell for windows

--- nvim_lazy/lua/config/options.lua
+++ nvim_lazy/lua/config/options.lua
@@ -82,3 +82,6 @@ local is_windows = vim.fn.has("win32") ~= 0
 local sep = is_windows and "\\" or "/"
 local delim = is_windows and ";" or ":"
 vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
+
+-- change shell to pwsh on windows
+if is_windows and vim.fn.executable("pwsh") == 1 then o.shell = "pwsh" end
