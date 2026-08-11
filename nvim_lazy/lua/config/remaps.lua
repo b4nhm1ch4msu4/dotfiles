bbc1572fb9c2b43e016c0f235a6626525d7063c7
Laptop HP <tienpham102vp@gmail.com>
2026-08-11 11:08:31 +0700

    add escape pair remap

--- nvim_lazy/lua/config/remaps.lua
+++ nvim_lazy/lua/config/remaps.lua
@@ -47,4 +47,26 @@ map("n", "<leader>yf", function()
 	vim.notify("Copied file name: " .. filename)
 end, { desc = "Copy file name" })
 
+-- move over a closing element in insert mode
+map("i", "<C-.>", function()
+  local closers = { ")", "]", "}", ">", "'", '"', "`", "," }
+  local line = vim.api.nvim_get_current_line()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local after = line:sub(col + 1, -1)
+  local closer_col = #after + 1
+  local closer_i = nil
+  for i, closer in ipairs(closers) do
+    local cur_index, _ = after:find(closer)
+    if cur_index and (cur_index < closer_col) then
+      closer_col = cur_index
+      closer_i = i
+    end
+  end
+  if closer_i then
+    vim.api.nvim_win_set_cursor(0, { row, col + closer_col })
+  else
+    vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+  end
+end, { desc = "move over a closing element" })
+
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
